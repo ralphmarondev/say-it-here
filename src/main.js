2f84c17b3b7b783470f139e0afbe673fdb2b9061
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-01-15 20:49:20 +0800

    added bootstrap

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import './style.css'
+import {createApp} from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import './assets/scss/app.scss'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+const app = createApp(App)
+app.mount('#app')
